8fc79f938bcbea070d0e47f99f9e167a1db58542
Aleksander Spro <abspro@gmail.com>
2017-09-05 12:09:53 +0200

    Add exmaple csv import files

--- GeoLite2Import/Program.cs
+++ GeoLite2Import/Program.cs
@@ -8,18 +8,14 @@ namespace GeoLite2Import
 {
     class Program
     {
-        static string Ipv4CititesImportFilePath = @"geolite2\GeoLite2-City-Blocks-IPv4.csv";
-        static string Ipv6CititesImportFilePath = @"geolite2\GeoLite2-City-Blocks-IPv6.csv";
-        static string Ipv4CountriesImportFilePath = @"geolite2\GeoLite2-Country-Blocks-IPv4.csv";
-        static string Ipv6CountriesImportFilePath = @"geolite2\GeoLite2-Country-Blocks-IPv6.csv";
-        static string WorldCitiesPopImportFilePath = @"geolite2\worldcitiespop.csv";
+        static string Ipv4CititesImportFilePath = @"geolite2\GeoLite2-City-Blocks-IPv4-example.csv";
+        static string Ipv4CountriesImportFilePath = @"geolite2\GeoLite2-Country-Blocks-IPv4-example.csv";
+        static string WorldCitiesPopImportFilePath = @"geolite2\worldcitiespop-example.csv";
 
         static void Main(string[] args)
         {
             LogImport<GeoLite2City>(Ipv4CititesImportFilePath);
-            LogImport<GeoLite2City>(Ipv6CititesImportFilePath);
             LogImport<GeoLite2Country>(Ipv4CountriesImportFilePath);
-            LogImport<GeoLite2Country>(Ipv6CountriesImportFilePath);
             LogImport<WorldCity>(WorldCitiesPopImportFilePath);
         }
 
